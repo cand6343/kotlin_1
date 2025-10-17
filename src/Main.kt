5460afdb6ed96cf5657c06e0361ecc0701c5a2d0
cand6123 <fjhdjdj876@gmail.com>
2025-10-17 20:30:23 +0300

    1 задание

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("2323sef")
+    print("Введите ваш уровень: ")
+    val level = readln().toInt()
+
+    if (level < 10) {
+        println("Новичок\nПрибавка к HP: +10")
+    } else if (level < 50) {
+        println("Опытный\nПрибавка к HP: +30")
+    } else {
+        println("Легенда\nПрибавка к HP: +100")
+    }
 }
