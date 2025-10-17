f11f0e06eb7cd9fb96dfcda89e49a521001af811
cand6123 <fjhdjdj876@gmail.com>
2025-10-17 20:30:23 +0300

    1 задание

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("2323sef")
+    print("Введите ваш возраст: ")
+    val age = readln().toInt()
+
+    if (age < 12) {
+        println("Вы ребёнок")
+    } else if (age <= 17) {
+        println("Вы подросток")
+    } else {
+        println("Вы взрослый")
+    }
 }
